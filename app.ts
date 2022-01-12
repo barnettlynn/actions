c04e0a0caed317feb4628f7cc9bff41757a30add
Lynn Barnett <barnettlynn@gmail.com>
2022-01-11 18:29:20 -0600

    remove the js file

--- app.ts
+++ app.ts
@@ -10,10 +10,22 @@ export function handler(event, context, callback) {
 }
 
 
+export function hello(event, context, callback) {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Hello, the current time is ${new Date().toTimeString()}. HELLO`,
+        }),
+      };
+      return response
+}
+
+
 console.log("Hello world")
 
 
 
 // https://www.serverless.com/framework/docs/providers/aws/guide/credentials
-
+// export AWS_ACCESS_KEY_ID=AKIAUACF2XUCAPTFZ2ZS
+// export AWS_SECRET_ACCESS_KEY=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
 
