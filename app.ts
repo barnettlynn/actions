51e7941dc62f35cd57538175ae11eb69db951cf9
Lynn Barnett <barnettlynn@gmail.com>
2022-01-11 19:21:43 -0600

    add types

--- app.ts
+++ app.ts
@@ -1,12 +1,16 @@
 
-export function handler(event, context, callback) {
-    const response = {
+
+import { APIGatewayProxyResultV2, APIGatewayProxyEventV2 } from "aws-lambda";
+
+export async function handler(event:APIGatewayProxyEventV2) : Promise<APIGatewayProxyResultV2> {
+
+    return  {
         statusCode: 200,
         body: JSON.stringify({
-          message: `Hello, LYNN.`,
+          message: `Hello, LYNN.`
         }),
       };
-      return response
+
 }
 
 
