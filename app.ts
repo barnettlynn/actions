d5c0e8f5e2eddae3f6ef3a685bf00d743025c5a8
Lynn Barnett <barnettlynn@gmail.com>
2022-01-11 18:54:53 -0600

    test reponsponse

--- app.ts
+++ app.ts
@@ -10,15 +10,15 @@ export function handler(event, context, callback) {
 }
 
 
-export function hello(event, context, callback) {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Hello, hello`,
-        }),
-      };
-      return response
-}
+// export function hello(event, context, callback) {
+//     const response = {
+//         statusCode: 200,
+//         body: JSON.stringify({
+//           message: `Hello, hello`,
+//         }),
+//       };
+//       return response
+// }
 
 
 
